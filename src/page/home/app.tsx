51eb9990da3f142cbae4eaa09e6ab535b628b912
subhonbek <basubhonbek@gmail.com>
2024-02-18 14:04:30 +0300

    fix: scroll

--- src/page/home/app.tsx
+++ src/page/home/app.tsx
@@ -5,7 +5,7 @@ import { PromoBlock } from '@/widgets/promo-block/promo-block';
 
 export function Home() {
   return (
-    <main className="grid items-center px-0">
+    <main className="grid items-center px-0 overflow-x-hidden">
       <PromoBlock />
       <Events />
       <FormSegment />
